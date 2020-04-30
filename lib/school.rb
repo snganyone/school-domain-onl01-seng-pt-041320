a7bd465859e5a7f4480ba2ac663e812f4b2636bd
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-04-29 22:00:24 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :grade
+    attr_reader :name, :roster
+
+    def initialize(name, roster = {})
+        @name = name
+        @roster = roster
+    end
+    
+    def add_student(name, grade)
+        #Checks if there is grade
+        if roster.key?(grade) == true
+            @roster[grade] << name
+        #If there is no grade a key value/pair needs to be assigned
+        else
+            roster[grade] = []
+            roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        return @roster.values_at(grade).flatten
+    end  
+
+    def sort
+        #new_hash = roster.sort.to_h
+        roster.each do |key|
+            puts key
+        end
+    end
+
+end
